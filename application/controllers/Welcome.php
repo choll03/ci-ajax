b58f11401cca1cebb318a9d198d1c5c5c068b577
Ismail <40472128+choll03@users.noreply.github.com>
2018-12-04 12:53:04 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-
+	//ini index
 	public function index()
 	{
 		$this->load->view('layout/header');
